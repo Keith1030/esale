fc9b08d8ed465462c660a56f55a36c70327cdbeb
user <user@MA324-48>
2017-03-10 16:06:14 +0800

    加入專案檔案。

---
+++ HelloWorld/Models/HelloApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class HelloApplication
+    {
+        public string GetDefaultMessage()
+        {
+            return "Hello world";
+        }
+    }
+}
