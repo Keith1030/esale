7e75a610b44922b636883aa8847e8f0b3b7078bc
user <user@MA324-48>
2017-03-10 16:22:57 +0800

    hello

--- HelloWorld/Controllers/OrderController.cs
+++ HelloWorld/Controllers/OrderController.cs
@@ -13,11 +13,7 @@ namespace HelloWorld.Controllers
         {
             Models.OrderService orderService = new Models.OrderService();
             var result = orderService.GetOrderById("001");
-            ViewBag.CustId = result.CustId;
-            ViewBag.CustName = result.CustName;
-            ViewBag.EmpId = result.EmpId;
-            ViewBag.EmpName = result.EmpName;
-            ViewBag.Orderdate = result.Orderdate;
+            ViewBag.order = result;
             return View();
         }
     }
